af0084012916f358cd317c74514c69281eaff4ab
yuhuijuan1 <yuhuijuan1@example.com>
2019-08-12 02:53:27 +0800

    Revert "4"
    This reverts commit 2ed4f73a

--- eureka-server/src/main/java/com/itmayiedu/app/EurekaApp.java
+++ eureka-server/src/main/java/com/itmayiedu/app/EurekaApp.java
@@ -10,7 +10,7 @@ public class EurekaApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApp.class, args);
-		System.out.print("a4rrrrrrrrrrrrrr4");
+		System.out.print("aaa");
 	}
 
 }
